6dfa050377fb1fd4c2deb7e7cc00609584a05f22
Raposo <william.s.godoy53@gmail.com>
2019-11-04 20:08:46 -0300

    Moving config model to database on config folder

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const currentMongoUrl = (process.env.NODE_ENV === 'test' && 'mongodb://127.0.0.1:27017/dooile-test') || process.env.DATABASE_URL
-console.log(process.env.NODE_ENV)
 
 module.exports =
   mongoose.connect(currentMongoUrl, {
