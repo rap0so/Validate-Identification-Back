f5c9eb17f98c5c05fbe5ab1125ea0b3c25bebdd2
Raposo <william.s.godoy53@gmail.com>
2019-11-04 23:44:16 -0300

    Updating to express v4

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
 const routes = require('./routes')
+const cors = require('cors')
 const connectDb = require('./config/database')
 const PORT = (process.env.NODE_ENV === 'test' && 5010) || 5003
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(express.json())
 
 routes.forEach(route => {
   app.use('/' + route.path, route.controller)
